7ea935a33d5e08f961eee99adc4a13f9eb6c7b8a
Mike Solomon <mike@wavr.so>
2022-04-08 12:19:39 +0300

    Open to other events

--- src/FRP/Event/Phantom.purs
+++ src/FRP/Event/Phantom.purs
@@ -4,30 +4,30 @@ import Prelude
 
 import Control.Alternative (class Alt, class Alternative, class Plus)
 import Data.Compactable (class Compactable)
-import FRP.Event (class Filterable, class IsEvent, Event)
+import FRP.Event (class Filterable, class IsEvent)
 
 data Proof0
 
-newtype PhantomEvent :: forall k. k -> Type -> Type
-newtype PhantomEvent phantom a = PhantomEvent (Event a)
+newtype PhantomEvent :: forall k. (Type -> Type) -> k -> Type -> Type
+newtype PhantomEvent event phantom a = PhantomEvent (event a)
 
-unsafePhantom :: forall proof a. Event a -> PhantomEvent proof a
+unsafePhantom :: forall event proof a. event a -> PhantomEvent event proof a
 unsafePhantom = PhantomEvent
 
-proof0 :: forall a. Event a -> PhantomEvent Proof0 a
+proof0 :: forall event a. event a -> PhantomEvent event Proof0 a
 proof0 = PhantomEvent
 
-toEvent :: forall proof a. PhantomEvent proof a -> Event a
+toEvent :: forall event proof a. PhantomEvent event proof a -> event a
 toEvent (PhantomEvent e) = e
 
-derive newtype instance Functor (PhantomEvent phantom)
-derive newtype instance Apply (PhantomEvent phantom)
-derive newtype instance Applicative (PhantomEvent phantom)
-derive newtype instance IsEvent (PhantomEvent phantom)
-derive newtype instance Filterable (PhantomEvent phantom)
-derive newtype instance Compactable (PhantomEvent phantom)
-derive newtype instance Alternative (PhantomEvent phantom)
-derive newtype instance Plus (PhantomEvent phantom)
-derive newtype instance Alt (PhantomEvent phantom)
-derive newtype instance Semigroup a => Semigroup (PhantomEvent phantom a)
-derive newtype instance Monoid a => Monoid (PhantomEvent phantom a)
+derive newtype instance Functor event => Functor (PhantomEvent event phantom)
+derive newtype instance Apply event => Apply (PhantomEvent event phantom)
+derive newtype instance Applicative event => Applicative (PhantomEvent event phantom)
+derive newtype instance IsEvent event => IsEvent (PhantomEvent event phantom)
+derive newtype instance Filterable event => Filterable (PhantomEvent event phantom)
+derive newtype instance Compactable event => Compactable (PhantomEvent event phantom)
+derive newtype instance Alternative event => Alternative (PhantomEvent event phantom)
+derive newtype instance Plus event => Plus (PhantomEvent event phantom)
+derive newtype instance Alt event => Alt (PhantomEvent event phantom)
+derive newtype instance Semigroup (event a) => Semigroup (PhantomEvent event phantom a)
+derive newtype instance Monoid (event a) => Monoid (PhantomEvent event phantom a)
